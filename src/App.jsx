9a52c931b3b0c01fc2c7e8a4fcc1221cbc9fba99
Eran Mahalu <seliko@gmail.com>
2026-06-16 21:09:38 +0300

    product details

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
-import './App.css'
-import Navbar from './components/Navbar';
-import AuthProvider from './context/AuthContext';
+import ProductDetails from "./pages/ProductDetails";
+import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthContext";
+import "./App.css";
 
 function App() {
-  
   return (
     <AuthProvider>
-   <div className='app'>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/auth" element={<Auth />}/>
-      <Route path="/checkout" element={<Checkout/>}/>
-    </Routes>
-    </div>
+      <div className="app">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </div>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom";
 
-export default function ProdactCard({product}) {
-    return(
-         <div className="product-card" >
-                            <img
-                                className="product-card-image"
-                                src={product.image}
-                                alt={product.name}
-                            />
+export default function ProductCard({ product }) {
+  return (
+    <div className="product-card">
+      <img
+        className="product-card-image"
+        src={product.image}
+        alt={product.name}
+      />
 
-                            <div className="product-card-content">
-                                <h3 className="product-card-name">{product.name}</h3>
-                                <p className="product-card-price">${product.price}</p>
+      <div className="product-card-content">
+        <h3 className="product-card-name">{product.name}</h3>
+        <p className="product-card-price">${product.price}</p>
 
-                                <div className="product-card-actions">
-                                    <Link className="btn btn-secondary">View details</Link>
-                                    <button className="btn btn-primary">Add to Cart</button>
-                                </div>
-                            </div>
-                        </div>
-    );
+        <div className="product-card-actions">
+          <Link className="btn btn-secondary" to={`/product/${product.id}`}>
+            View details
+          </Link>
+
+          <button className="btn btn-primary">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
 }
